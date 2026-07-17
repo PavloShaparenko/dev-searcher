d63b96a17c48e9cb1ebec6ea5ff97bc76c03163f
Pavel Shap. <shap.pavel2007@gmail.com>
2026-07-17 23:18:43 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+
+import { Container } from './components/Container';
+import { TheHeader } from './components/TheHeader';
+import { Search } from './components/Search/Search';
+import { GithubError, GithubUser, LocalGithubUser } from './types/index';
+import { UserCard } from './components/UserCard';
+import { isGithubUser } from './utils/typeguards';
+import { extractLocalUser } from './utils/exract-local-user';
+
+import { defaultUser } from './mock';
+
+const BASE_URL = 'https://api.github.com/users/';
+
+function App() {
+  const [user, setUser] = useState<LocalGithubUser | null>(defaultUser);
+
+  const fetchUser = async (username: string) => {
+    const url = BASE_URL + username;
+
+    const res = await fetch(url);
+    const user = await res.json() as GithubUser | GithubError;
+
+    if (isGithubUser(user)) {
+      setUser(extractLocalUser(user));
+    } else {
+      setUser(null);
+    }
+  }
+
+  return (
+    <Container>
+      <TheHeader />
+      <Search hasError={!user} onSubmit={fetchUser}/>
+      {user && (
+        <UserCard
+          {...user}
+        />
+      )}
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser} from "../types/user";
+
+export const defaultUser: LocalGithubUser = {
+  "login": "PavloShaparenko",
+  "avatar": "https://avatars.githubusercontent.com/u/151019903?v=4",
+  "name": "Pavel_Shaparenko",
+  "company": "none",
+  "blog": "mishanep.com",
+  "location": "Lublin",
+  "bio": "student of KUL",
+  "twitter": "P2bL0_S",
+  "repos": 60,
+  "followers": 1,
+  "following": 3,
+  "created": "2023-11-16T09:44:42Z",
+}
